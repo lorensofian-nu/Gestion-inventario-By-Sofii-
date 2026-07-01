5d66b4af68fa4e456ff19b13c88cc401c1c17849
Loren nunez <lorensofian23@gmail.com>
2026-06-30 21:00:21 -0500

    fix:arregle usuario

--- usuario.js
+++ usuario.js
@@ -1,15 +1,28 @@
 const URL_BASE = "https://stock-flow-e92c1-default-rtdb.firebaseio.com"
-const formulario = document.getElementById("formulario"); 
+const formulario = document.getElementById("formulario");
 
-const httpClient= fetch(`${URL_BASE}/user.json`,{
-  method:"POST",
-  header:{
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(user)
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = new FormData(formulario);
+  const user = {
+    "username": data.get(username),
+    "name": data.get("name"),
+    "password": data.get("password")
+  }
+  const res = httpClient(user).then(data => data.json());
+  res.then(data => console.log).catch(err => console.error)
 });
 
-const res= httpClient.then(data=> data.json());
-res.then(data=> console.log).catch(err=> console.error)
+const httpClient = (user) => {
+  fetch(`${URL_BASE}/user.json`, {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+  });
+}
+
+
 
 
