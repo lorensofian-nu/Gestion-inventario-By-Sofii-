0d6f59e8f5f097f19650be2c18b8b0ff96bb1962
sofia nunez <lorensofian23@gmail.com>
2026-07-04 23:52:42 -0500

    feat:agregue validacion inicio sesion....

--- js/login.js
+++ js/login.js
@@ -1,14 +1,44 @@
 const URL_BASE = "https://stock-flow-e92c1-default-rtdb.firebaseio.com";
+const formulario = document.getElementById("formulario");
 
-const botonInicio = document.getElementById("IniciarSesion");
-
-botonInicio.addEventListener("click", (event) => {
-
+formulario.addEventListener("submit", async (event) => {
     event.preventDefault();
+    const data = new FormData(formulario);
+    const nombreIngresado = data.get("nombre").trim();
+    const identificacionIngresada = data.get("identificacion").trim();
+    const passwordIngresada = data.get("password");
 
-    const identificacion = document.getElementById("identificacion").value;
-    const contraseña = document.getElementById("password").value;
-    if (!identificacion || !contraseña) {
-         mensaje.textContent = "Por favor, llena todos los campos.";
+    if (!nombreIngresado || !identificacionIngresada || !passwordIngresada) {
+        alert("Por favor, rellene todos los campos.");
         return;
     }
+
+    const respuesta = await fetch(`${URL_BASE}/usuarios.json`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    const listaUsuarios = await respuesta.json();
+
+    if (!listaUsuarios) {
+        alert("No hay usuarios registrados en el sistema.");
+        return;
+    }
+
+    const arrayUsuarios = Object.values(listaUsuarios);
+
+    const usuarioEncontrado = arrayUsuarios.find(usuario =>
+        usuario.nombre.trim().toLowerCase() === nombreIngresado.toLowerCase() &&
+        usuario.identificacion.toString().trim() === identificacionIngresada &&
+        usuario.password === passwordIngresada
+    );
+
+    if (usuarioEncontrado) {
+        alert(`¡Bienvenido al sistema, ${usuarioEncontrado.nombre}!`);
+        window.location.href = "../pages/registrar.html";
+    } else {
+        alert("Los datos ingresados no coinciden power ranger.");
+    }
+});
